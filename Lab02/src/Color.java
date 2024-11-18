17fc99643f0104413111fd66dbd3b5f214ecc63a
Jakub Korytko <jakub@korytko.me>
2024-11-18 14:55:24 +0100

    Add Lab02 task 2 solution

--- Lab02/src/Color.java
+++ Lab02/src/Color.java
@@ -1,21 +1,25 @@
-public record Color(int red, int green, int blue) {
+public record Color(int red, int green, int blue, int alpha) {
     public Color  {
-        boolean lessThan0 = red < 0 ||  green < 0 ||  blue < 0;
-        boolean greaterThan255 = red > 255 || green > 255 || blue > 255;
+        boolean lessThan0 = red < 0 ||  green < 0 ||  blue < 0 || alpha < 0;
+        boolean greaterThan255 = red > 255 || green > 255 || blue > 255 || alpha > 255;
         if (greaterThan255 || lessThan0) {
             throw new IllegalArgumentException("Invalid color");
         }
     }
 
     public Color() {
-        this(0, 0, 0);
+        this(0, 0, 0, 0);
     }
 
     public Color(int red) {
-        this(red, 0, 0);
+        this(red, 0, 0, 0);
     }
 
     public Color(int red, int green) {
-        this(red, green, 0);
+        this(red, green, 0, 0);
+    }
+
+    public Color(int red, int green,  int blue) {
+        this(red, green, blue, 0);
     }
 }
