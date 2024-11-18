6d46ded649e904e21b2bc215de53448060094aee
Jakub Korytko <jakub@korytko.me>
2024-11-18 16:05:01 +0100

    Add Lab03 task 3 solution

--- Lab03/src/main/java/org/example/Main.java
+++ Lab03/src/main/java/org/example/Main.java
@@ -5,8 +5,19 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Color red = new Color(255, 0, 0, 255);
-        Triangle redTriangle =  new  Triangle(5, 10,  5, red);
-        redTriangle.print();
-        System.out.println(redTriangle.getColorDescription());
+        Color blue = new Color(0, 0, 255, 255);
+        Color yellow = new Color(255, 255, 0, 255);
+
+        Triangle redTriangle =  new Triangle(5, 10,  5, red);
+        Triangle yellowTriangle =  new Triangle(4, 8,  9, yellow);
+
+        ShapeDescriber.describe(redTriangle);
+        ShapeDescriber.describe(yellowTriangle);
+
+        Rectangle blueRectangle = new Rectangle(10, 15, blue);
+        Rectangle yellowRectangle = new Rectangle(5, 10, yellow);
+
+        ShapeDescriber.describe(blueRectangle);
+        ShapeDescriber.describe(yellowRectangle);
     }
 }
